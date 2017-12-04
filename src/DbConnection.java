2d532b5be996caff374e6a9210207185448860d0
gwib <gwib@users.noreply.github.com>
2017-12-04 13:10:39 +0100

    finally established connection to mysql db

--- src/DbConnection.java
+++ src/DbConnection.java
@@ -4,14 +4,16 @@ import java.sql.*;
 public class DbConnection {
 
     // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "org.sqlite.JDBC";
-    static final String DB_URL = "jdbc:sqlite:rmi.sqlite";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/main";
+    String USER = "root";
+    String PASS = "";
 
     public Connection connectToDb(){
         Connection conn = null;
         try {
             // Register JDBC drive
-            Class.forName("org.sqlite.JDBC");
+            Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(DB_URL);
             System.out.println("Connection to DB established");
             return conn;
